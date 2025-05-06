527a86cb494a46048fbf22c2b1d968dcfe585443
srtopuzova <siyana.r.topuzova.2023@elsys-bg.org>
2025-05-06 18:39:52 +0300

    Added Article serializer and ListCreateAPIView endpoint

--- djangoproject/articles/views.py
+++ djangoproject/articles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Article
+from .serializers import ArticleSerializer
 
 # Create your views here.
+
+class ArticleListView(generics.ListCreateAPIView):
+    queryset = Article.objects.all().order_by('-created_at')
+    serializer_class = ArticleSerializer

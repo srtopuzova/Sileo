3cab1b1b0619ed39772c23faa68bd1e7fedbfd1e
srtopuzova <siyana.r.topuzova.2023@elsys-bg.org>
2025-05-26 11:00:13 +0300

    testing

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'articles',
     'rest_framework.authtoken',
     'django_filters',
+    'articles',
     'users'
 ]
 
